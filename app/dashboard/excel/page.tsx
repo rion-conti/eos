6e72ab32e3ac4a2f256e03756443b25724d76152
rionconti <rionconti@yahoo.com>
2026-07-08 22:18:13 +0800

    initial commit

--- app/dashboard/excel/page.tsx
+++ app/dashboard/excel/page.tsx
@@ -1,15 +1,21 @@
-import { Suspense } from "react";
-import CustomerSection from "@/components/excel/customer-section";
-import H1 from "@/components/H1";
+import { Suspense } from "react"
+import CustomerSection from "@/components/excel/customer-section"
+import H1 from "@/components/H1"
 
 export default async function ExportPage() {
   return (
     <main className="p-8">
       <H1>Customer Management</H1>
 
-      <Suspense fallback={<div className="mt-6 text-sm text-green-500">Loading export button...</div>}>
+      <Suspense
+        fallback={
+          <div className="mt-8 text-sm text-green-500">
+            Loading export button...
+          </div>
+        }
+      >
         <CustomerSection />
       </Suspense>
     </main>
-  );
+  )
 }
